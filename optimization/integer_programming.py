0df15d5235b9d721d4ea2910910d303b12bcdc23
Hardy Hasan <Harady.Hasan@gmail.com>
2023-11-23 16:49:20 +0100

    Continuing the implementation of the integer prog model.

--- optimization/integer_programming.py
+++ optimization/integer_programming.py
@@ -8,9 +8,9 @@ Given a list of operational intents, this model creates a list for their earlies
 as well as a list of all edges, and a list of time steps.
 
 The model has a binary decision variable for each edge, drone and time step, where the variable has value 1
-if drone d traverses that edge at time t.
+if drone `d` traverses that edge at time `t`.
 Likewise, a binary decision variable exists for each vertiport, intent and time step, where it has value 1 if
-vertiport i is reserved for drone d at time t.
+vertiport `i` is reserved for drone d at time `t`.
 
 There are five constraints to be satisfied:
 1. Flow conservation: a drone entering a vertiport must also leave it, except the source and destination
@@ -23,10 +23,100 @@ There are five constraints to be satisfied:
 
 The objective of the model is to find the shortest total time of operation for all intents.
 """
-
+import itertools
 import mip
 
-model = mip.Model(solver_name="gurobi")
-print(model)
+
+def calculate_edge_weight(weight: int, time_delta: int) -> int:
+    """
+    This function calculates the corresponding time discretized weight of an edge given its original weight.
+
+    Parameters
+    ----------
+    weight: int
+        Original weight of an edge in minutes.
+    time_delta: int
+        Time discretization step.
+
+    Returns
+    -------
+    weight: int
+        New weight
+
+    """
+    k, r = divmod(weight, time_delta)
+    weight = (k + int(r > 0)) * time_delta
+
+    return weight
+
+
+def ip_optimization(nodes: dict, edges: dict, intents: dict, time_steps: range, time_delta: int) -> float:
+    """
+    This function runs an optimization model to find routing solution for each operational intent.
+
+    Parameters
+    ----------
+    nodes: dict
+        Dictionary of the nodes in a graph and their names.
+    edges: dict
+        Dictionary of the edges in a graph and their names.
+    intents: dict
+        Dictionary of the operational intents to be scheduled and their names.
+    time_steps: range
+        A range object for the discretized time steps.
+    time_delta: int
+        Time discretization step.
+
+    Returns
+    -------
+    ip_obj: float
+        The objective of the model
+    """
+    ip_obj = 0
+
+    model = mip.Model(name="balance", sense=mip.MINIMIZE, solver_name=mip.GUROBI)
+
+    # ---- Constants ----
+    vertiports_list = list(nodes.keys())
+    vertiports_ids = range(len(vertiports_list))
+    edges_list = list(edges.keys())
+    edges_ids = range(len(edges_list))
+    drones_list = list(intents.keys())
+    drones_ids = range(len(drones_list))
+    time_steps_ids = [i for i in range(len(time_steps))]
+    earliest_departure_times = [intent.start for intent in intents.values()]
+    edge_weights = [calculate_edge_weight(edge.weight, time_delta) for edge in edges.values()]
+
+    # ---- Decision variables ----
+    # whether edge e is traversed by drone d starting at time step t
+    drones_path = [[[model.add_var(name=f"{e}{d}{t}", var_type=mip.BINARY)
+                     for t in time_steps]
+                    for d in drones_list]
+                   for e in edges_list]
+
+    # whether vertiport v is reserved for drone d starting at time step t
+    vertiport_reserved = [[[model.add_var(name=f"{v}{d}{t}", var_type=mip.BINARY)
+                            for t in time_steps]
+                           for d in drones_ids]
+                          for v in vertiports_ids]
+
+    # ---- Objective ----
+    # minimizing the total sum of times of all drones schedules.
+    model.objective = mip.xsum(drones_path[e][d][t] * edge_weights[e]
+                               for e in edges_ids for d in drones_ids for t in time_steps_ids
+                               )
+    model.optimize()
+
+    # ---- Constraints ----
+
+    # ---- Output ----
+    # checking if a solution was found
+    if model.num_solutions:
+        ip_obj = model.objective_value
+        print(f"ip_obj={ip_obj}")
+        # for idx, el in enumerate(edges_drones_timesteps_product):
+        #     print(f"{el}: {drones_path[idx].x}")
+
+    return ip_obj
 
 # =============================================== END OF FILE ===============================================
