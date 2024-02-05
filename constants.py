46ccec089dfd211be0bbcc84de8b1c498328b3b7
Hardy Hasan <Harady.Hasan@gmail.com>
2024-02-05 16:05:39 +0300

    New file for defining constants used across the project.

---
+++ constants.py
@@ -0,0 +1,9 @@
+"""
+File description:
+-----------------
+This file contains constant values that can be used across the project files.
+"""
+
+MAXIMUM_RUNTIME = 12*60*60  # time in seconds an ip model is allowed to do solving.
+
+# =============================================== END OF FILE ===============================================
