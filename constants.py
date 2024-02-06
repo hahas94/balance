1bfa029ec7d39be792a7a7a546929b8e4439c51f
Hardy Hasan <Harady.Hasan@gmail.com>
2024-02-06 16:44:28 +0300

    New constant added.

--- constants.py
+++ constants.py
@@ -4,6 +4,10 @@ File description:
 This file contains constant values that can be used across the project files.
 """
 
-MAXIMUM_RUNTIME = 12*60*60  # time in seconds an ip model is allowed to do solving.
+# --- Time in seconds an ip model is allowed to do solving. ---
+MAXIMUM_RUNTIME = 12*60*60
+
+# --- If a model reaches this gap, then stop optimizing further. This is used to speed up runtime of models. ---
+ALLOWED_GAP = 0.1
 
 # =============================================== END OF FILE ===============================================
