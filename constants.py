ea13f0b7b00bd61aa8f6406f02c9d4468d569ce0
Hardy Hasan <Harady.Hasan@gmail.com>
2024-02-08 20:05:43 +0300

    adjusting constants.

--- constants.py
+++ constants.py
@@ -8,6 +8,6 @@ This file contains constant values that can be used across the project files.
 MAXIMUM_RUNTIME = 12*60*60
 
 # --- If a model reaches this gap percentage, stop further optimization. This is used to speed up solving runtime. ---
-ALLOWED_GAP = 0.10
+ALLOWED_GAP = 0.05
 
 # =============================================== END OF FILE ===============================================
