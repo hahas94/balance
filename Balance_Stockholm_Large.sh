cc991cbc433642dfe8393a4dc4a27896e99fb24f
Hardy Hasan <Harady.Hasan@gmail.com>
2024-02-08 14:18:13 +0300

    Adjusting shell files.

--- Balance_Stockholm_Large.sh
+++ Balance_Stockholm_Large.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-#SBATCH -t 14:00:00
+#SBATCH -t 24:00:00
 #SBATCH -J Balance_Stockholm_Large
 #SBATCH -N 1
 #SBATCH --exclusive
-#SBATCH -o "~/balance/results/stockholm_large/logs.txt"
-#SBATCH -e "~/balance/results/stockholm_large/logs.txt"
 #SBATCH --mail-user=hardy.hasan@liu.se
 #SBATCH --mail-type=ALL
 
@@ -13,4 +11,4 @@ module load gurobi/9.0.0-nsc1
 module load Python/3.10.4-env-hpc1-gcc-2022a-eb
 source venv/bin/activate
 
-python3 main.py
+python3 main.py --graph_name=stockholm_large > results/stockholm_large/logs.txt
--- Balance_Stockholm_Medium.sh
+++ Balance_Stockholm_Medium.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-#SBATCH -t 00:05:00
+#SBATCH -t 24:00:00
 #SBATCH -J Balance_Stockholm_Medium
 #SBATCH -N 1
 #SBATCH --exclusive
-#SBATCH -o results/stockholm_medium/logs.txt
-#SBATCH -e results/stockholm_medium/logs.txt
 #SBATCH --mail-user=hardy.hasan@liu.se
 #SBATCH --mail-type=ALL
 
@@ -13,4 +11,4 @@ module load gurobi/9.0.0-nsc1
 module load Python/3.10.4-env-hpc1-gcc-2022a-eb
 source venv/bin/activate
 
-python main.py > results/stockholm_medium/logs.txt
+python3 main.py --graph_name=stockholm_medium > results/stockholm_medium/logs.txt
