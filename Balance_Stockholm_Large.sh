7a6beb43f85d27de9b14713a8258fe9594ffcc6e
Hardy Hasan <Harady.Hasan@gmail.com>
2024-02-08 20:04:55 +0300

    Adding flexibility to pass arguments from cli.

--- Balance_Stockholm_Large.sh
+++ Balance_Stockholm_Large.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH -t 24:00:00
-#SBATCH -J Balance_Stockholm_Large
+#SBATCH -J Balance_Stockholm_Large_$1
 #SBATCH -N 1
 #SBATCH --exclusive
 #SBATCH --mail-user=hardy.hasan@liu.se
@@ -11,4 +11,4 @@ module load gurobi/9.0.0-nsc1
 module load Python/3.10.4-env-hpc1-gcc-2022a-eb
 source venv/bin/activate
 
-python3 main.py --graph_name=stockholm_large > results/stockholm_large/logs.txt
+python3 main.py --graph_name=stockholm_large --run_name="$1" --num_intents="$1" > results/stockholm_large/logs_"$1".txt
