87fdb03fe43dc6044c9ce08c3df9760d678ef2cc
Hardy Hasan <Harady.Hasan@gmail.com>
2024-02-08 14:18:38 +0300

    Adjusting constants.

--- constants.py
+++ constants.py
@@ -7,7 +7,7 @@ This file contains constant values that can be used across the project files.
 # --- Time in seconds an ip model is allowed to do solving. ---
 MAXIMUM_RUNTIME = 12*60*60
 
-# --- If a model reaches this gap, then stop optimizing further. This is used to speed up runtime of models. ---
-ALLOWED_GAP = 0.1
+# --- If a model reaches this gap percentage, stop further optimization. This is used to speed up solving runtime. ---
+ALLOWED_GAP = 0.10
 
 # =============================================== END OF FILE ===============================================
