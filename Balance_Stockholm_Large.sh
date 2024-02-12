3817d3ad9036c6f1868fbb733abb1389219efee5
Hardy Hasan <Harady.Hasan@gmail.com>
2024-02-12 08:13:25 +0100

    Changing sbatch parameters.

--- Balance_Stockholm_Large.sh
+++ Balance_Stockholm_Large.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #SBATCH -t 24:00:00
-#SBATCH -J Balance_Stockholm_Large_$1
+#SBATCH -J Balance_Stockholm_Large
 #SBATCH -N 1
+#SBATCH --mem=128000
 #SBATCH --exclusive
 #SBATCH --mail-user=hardy.hasan@liu.se
 #SBATCH --mail-type=ALL
@@ -11,4 +12,4 @@ module load gurobi/9.0.0-nsc1
 module load Python/3.10.4-env-hpc1-gcc-2022a-eb
 source venv/bin/activate
 
-python3 main.py --graph_name=stockholm_large --run_name="$1" --num_intents="$1" > results/stockholm_large/logs_"$1".txt
+python3 main.py --graph_name=stockholm_large --run_name="$1" --num_intents="$1" > results/stockholm_large_"$1"/logs.txt
--- Balance_Stockholm_Medium.sh
+++ Balance_Stockholm_Medium.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #SBATCH -t 24:00:00
-#SBATCH -J Balance_Stockholm_Medium_$1
+#SBATCH -J Balance_Stockholm_Medium
 #SBATCH -N 1
+#SBATCH --mem=16000
 #SBATCH --exclusive
 #SBATCH --mail-user=hardy.hasan@liu.se
 #SBATCH --mail-type=ALL
@@ -11,4 +12,4 @@ module load gurobi/9.0.0-nsc1
 module load Python/3.10.4-env-hpc1-gcc-2022a-eb
 source venv/bin/activate
 
-python3 main.py --graph_name=stockholm_medium --run_name="$1" --num_intents="$1" > results/stockholm_medium/logs_"$1".txt
+python3 main.py --graph_name=stockholm_medium --run_name="$1" --num_intents="$1" > results/stockholm_medium_"$1"/logs.txt
