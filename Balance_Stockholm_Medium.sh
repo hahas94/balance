30ae92244800f74ef6ae29d54e0b013477858527
Hardy Hasan <Harady.Hasan@gmail.com>
2024-02-15 11:35:40 +0300

    Adding new commands for creating directories.

--- Balance_Stockholm_Medium.sh
+++ Balance_Stockholm_Medium.sh
@@ -12,4 +12,8 @@ module load gurobi/9.0.0-nsc1
 module load Python/3.10.4-env-hpc1-gcc-2022a-eb
 source venv/bin/activate
 
+mkdir results/stockholm_medium_"$1"
+mkdir results/stockholm_medium_"$1/models"
+touch results/stockholm_medium_"$1/logs.txt"
+
 python3 main.py --graph_name=stockholm_medium --random_intents --run_name="$1" --num_intents="$1" > results/stockholm_medium_"$1"/logs.txt
